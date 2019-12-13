29a8eb6090755abf80ae8a4ca0332e81755b2297
biotooff <>
2019-12-13 22:27:48 +0800

    GO111MOUDLE=off

--- build/build-linux&win@64.sh
+++ build/build-linux&win@64.sh
@@ -9,9 +9,9 @@ spec="server client"
 fi
 
 if [[ $spec =~ "client" ]];then
-env CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o client_windows_64.exe github.com/biotooff/kcptun/client
+env GO111MODULE=off CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o client_windows_64.exe github.com/biotooff/kcptun/client
 fi 
 
 if [[ $spec =~ "server" ]];then
-env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o server_linux_server github.com/biotooff/kcptun/server
+env GO111MODULE=off CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o server_linux_server github.com/biotooff/kcptun/server
 fi
